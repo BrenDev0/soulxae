6d7f842e1fc561fd67fe29e5fa1bde9471f89496
BrenDev0 <lahey1991@gmail.com>
2025-06-16 09:58:22 -0600

    webhook route

--- dist/src/modules/whatsapp/whatsapp.routes.js
+++ dist/src/modules/whatsapp/whatsapp.routes.js
@@ -1,31 +1,37 @@
 "use strict";
-// import { Router } from 'express';
-// import Container from '../../core/dependencies/Container';
-// import MiddlewareService from '../../core/middleware/MiddlewareService';
-// import WhatsappController from './WhatsappController';
-// export const initializeWhatsappRouter = (customController?: WhatsappController) => {
-//     const router = Router();
-//     const secureRouter = Router();
-//     const middlewareService = Container.resolve<MiddlewareService>("MiddlewareService");
-//     const controller = customController ?? Container.resolve<WhatsappController>("WhatsappController");
-//     secureRouter.use(middlewareService.auth.bind(middlewareService));
-//      /*
-//         #swagger.tags = ['Whatsapp']
-//         #swagger.path =  '/whatsapp/secure'
-//         #swagger.security = [{ "bearerAuth": [] }] 
-//         #swagger.description = 'Update whatsapp'
-//         #swagger.requestBody = {
-//             required: true,
-//             content: {
-//                 "application/json": {
-//                     schema: { $ref: "#/components/schemas/updateWhatsapp" }
-//                 }
-//             }
-//         }
-//         */
-//     // protected Routes //
-//     // mounts //
-//     router.use("/secure", secureRouter);
-//     console.log("Whatsapp router initialized.");
-//     return router;
-// }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.initializeWhatsappRouter = void 0;
+const express_1 = require("express");
+const Container_1 = __importDefault(require("../../core/dependencies/Container"));
+const initializeWhatsappRouter = (customController) => {
+    const router = (0, express_1.Router)();
+    const secureRouter = (0, express_1.Router)();
+    const middlewareService = Container_1.default.resolve("MiddlewareService");
+    const controller = customController !== null && customController !== void 0 ? customController : Container_1.default.resolve("WhatsappController");
+    secureRouter.use(middlewareService.auth.bind(middlewareService));
+    /*
+       #swagger.tags = ['Whatsapp']
+       #swagger.path =  '/whatsapp/secure'
+       #swagger.security = [{ "bearerAuth": [] }]
+       #swagger.description = 'Update whatsapp'
+       #swagger.requestBody = {
+           required: true,
+           content: {
+               "application/json": {
+                   schema: { $ref: "#/components/schemas/updateWhatsapp" }
+               }
+           }
+       }
+       */
+    // protected Routes //
+    // unprotected routes //
+    router.get('/:id/webhook', controller.verifyWebhook.bind(controller));
+    // mounts //
+    router.use("/secure", secureRouter);
+    console.log("Whatsapp router initialized.");
+    return router;
+};
+exports.initializeWhatsappRouter = initializeWhatsappRouter;
--- src/modules/whatsapp/whatsapp.routes.ts
+++ src/modules/whatsapp/whatsapp.routes.ts
@@ -1,40 +1,41 @@
-// import { Router } from 'express';
-// import Container from '../../core/dependencies/Container';
-// import MiddlewareService from '../../core/middleware/MiddlewareService';
-// import WhatsappController from './WhatsappController';
+import { Router } from 'express';
+import Container from '../../core/dependencies/Container';
+import MiddlewareService from '../../core/middleware/MiddlewareService';
+import WhatsappController from './WhatsappController';
 
-// export const initializeWhatsappRouter = (customController?: WhatsappController) => {
-//     const router = Router();
-//     const secureRouter = Router();
-//     const middlewareService = Container.resolve<MiddlewareService>("MiddlewareService");
-//     const controller = customController ?? Container.resolve<WhatsappController>("WhatsappController");
+export const initializeWhatsappRouter = (customController?: WhatsappController) => {
+    const router = Router();
+    const secureRouter = Router();
+    const middlewareService = Container.resolve<MiddlewareService>("MiddlewareService");
+    const controller = customController ?? Container.resolve<WhatsappController>("WhatsappController");
 
-//     secureRouter.use(middlewareService.auth.bind(middlewareService));
+    secureRouter.use(middlewareService.auth.bind(middlewareService));
 
-//      /*
-//         #swagger.tags = ['Whatsapp']
-//         #swagger.path =  '/whatsapp/secure'
-//         #swagger.security = [{ "bearerAuth": [] }] 
-//         #swagger.description = 'Update whatsapp'
-//         #swagger.requestBody = {
-//             required: true,
-//             content: {
-//                 "application/json": {
-//                     schema: { $ref: "#/components/schemas/updateWhatsapp" }
-//                 }
-//             }
-//         }
-//         */
+     /*
+        #swagger.tags = ['Whatsapp']
+        #swagger.path =  '/whatsapp/secure'
+        #swagger.security = [{ "bearerAuth": [] }] 
+        #swagger.description = 'Update whatsapp'
+        #swagger.requestBody = {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: { $ref: "#/components/schemas/updateWhatsapp" }
+                }
+            }
+        }
+        */
 
-//     // protected Routes //
+    // protected Routes //
 
 
-  
+  // unprotected routes //
+  router.get('/:id/webhook', controller.verifyWebhook.bind(controller));
 
-//     // mounts //
+    // mounts //
+    
+    router.use("/secure", secureRouter);
 
-//     router.use("/secure", secureRouter);
-
-//     console.log("Whatsapp router initialized.");
-//     return router;
-// }
+    console.log("Whatsapp router initialized.");
+    return router;
+}
