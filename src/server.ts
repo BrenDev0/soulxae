51ce020f4072f23f35a8c3c11e476b0a8dbe7d43
BrenDev0 <lahey1991@gmail.com>
2025-07-29 16:21:42 -0600

    typo in conversations route

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ const server = async() => {
     app.use("/ai-tools", aiToolsRouter);
     app.use("/agents", agentsRouter);
     app.use("/clients", clientsRouter);
-    app.use("/conversatations", conversationsRouter);
+    app.use("/conversations", conversationsRouter);
     app.use("/direct", directMessagingRouter);
     app.use("/employees", employeesRouter);
     app.use("/flow-config", flowConfigRouter);
